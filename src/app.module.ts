d8e0e4633397cab70eb4d62ea39c9f852b4a3f2e
Nguyen Duc Thinh <thinhdz1500@gmail.com>
2025-02-21 15:55:40 +0700

    project config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ConfigModule.forRoot({
+    //không cần import ConfigModule.forRoot() trong UsersModule vì nó đã được import ở AppModule
+    isGlobal: true
+  }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
