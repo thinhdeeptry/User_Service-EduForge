8c0394b5ae39b5a528c65faca124bef35824d975
Nguyen Duc Thinh <thinhdz1500@gmail.com>
2025-02-22 22:36:16 +0700

    add function update user

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -60,8 +60,11 @@ export class UsersService {
     return `This action returns a #${id} user`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update( updateUserDto: UpdateUserDto) {
+    return await this.userModel.updateOne(
+      {_id: updateUserDto._id}
+      ,{...updateUserDto}
+    );
   }
 
   remove(id: number) {
